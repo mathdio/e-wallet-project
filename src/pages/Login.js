0552e094e1feb55baa39775317bd1b1ac4cfc23b
matheus <mathdiogenes@gmail.com>
2022-09-29 16:40:56 -0300

    Requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,78 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loginAction } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    buttonDisabled: true,
+  };
+
+  checkForm = () => {
+    const { email, password } = this.state;
+    const passwordLength = 6;
+    const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    if (emailRegex.test(email) && password.length >= passwordLength) {
+      this.setState({ buttonDisabled: false });
+    } else {
+      this.setState({ buttonDisabled: true });
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.checkForm();
+    });
+  };
+
+  submitEmail = (e) => {
+    e.preventDefault();
+    const { email } = this.state;
+    const { handleLogin, history } = this.props;
+    handleLogin(email);
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, buttonDisabled } = this.state;
+    return (
+      <form onSubmit={ this.submitEmail }>
+        <input
+          name="email"
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChange }
+        />
+        <input
+          name="password"
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit"
+          disabled={ buttonDisabled }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  handleLogin: (email) => dispatch(loginAction(email)),
+});
+
+Login.propTypes = {
+  handleLogin: PropTypes.string,
+}.isRequired;
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,16 @@
-// configure aqui sua store
+import { composeWithDevTools } from '@redux-devtools/extension/';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+if (window.Cypress) {
+  window.store = store;
+}
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+export default store;
