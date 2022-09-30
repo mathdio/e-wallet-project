f0ec6b8c63b70d3285ddc40c03300f9da9adb4b3
matheus <mathdiogenes@gmail.com>
2022-09-30 17:42:56 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import uuid from 'react-uuid';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table>
         <tr>
@@ -15,9 +19,39 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        <tbody>
+          {expenses.map((expense) => {
+            const conversion = Number(expense.exchangeRates[expense.currency].ask)
+            * Number(expense.value);
+            const fixedConversion = Number(conversion.toFixed(2));
+            const fixedValue = Number(expense.value).toFixed(2);
+            const fixedExchange = Number(expense.exchangeRates[expense.currency].ask)
+              .toFixed(2);
+            return (
+              <tr key={ uuid() }>
+                <td>{expense.description}</td>
+                <td>{expense.tag}</td>
+                <td>{expense.method}</td>
+                <td>{fixedValue}</td>
+                <td>{expense.exchangeRates[expense.currency].name}</td>
+                <td>{fixedExchange}</td>
+                <td>{fixedConversion}</td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.array,
+}.isRequired;
+
+export default connect(mapStateToProps)(Table);
