6a59406beb841fda032f5d368eae424b8c76f19f
matheus <mathdiogenes@gmail.com>
2022-10-03 16:13:32 -0300

    Requisito 10

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,36 +1,55 @@
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
-import Login from '../pages/Login';
+import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
-describe('testes da página de Login', () => {
-  beforeEach(() => {
-    renderWithRouterAndRedux(<Login />);
-  });
+const EMAIL_TEST = 'teste@teste.com';
+const PASSWORD_INPUT_TESTID = 'password-input';
 
+describe('testes da página de Login', () => {
   it('testa se o botão está desabilitado quando a página carrega', () => {
+    renderWithRouterAndRedux(<App />);
+
     const loginButton = screen.getByRole('button', { name: /entrar/i });
     expect(loginButton).toBeDisabled();
 
     const emailInput = screen.getByRole('textbox');
-    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(emailInput, EMAIL_TEST);
 
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT_TESTID);
     userEvent.type(passwordInput, '123456');
 
     expect(loginButton).toBeEnabled();
   });
 
   it('teste se o botão é habilitado quando os campos de email e senha possuem valores válidos', () => {
+    renderWithRouterAndRedux(<App />);
+
     const loginButton = screen.getByRole('button', { name: /entrar/i });
 
     const emailInput = screen.getByRole('textbox');
-    userEvent.type(emailInput, 'teste@teste.com');
+    userEvent.type(emailInput, EMAIL_TEST);
 
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT_TESTID);
     userEvent.type(passwordInput, '123456');
 
     expect(loginButton).toBeEnabled();
+  });
+
+  it('testa se a página é redirecionada par a apágina de carteira quando fizer o login', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+
+    const emailInput = screen.getByRole('textbox');
+    userEvent.type(emailInput, EMAIL_TEST);
+
+    const passwordInput = screen.getByTestId(PASSWORD_INPUT_TESTID);
+    userEvent.type(passwordInput, '123456');
+
+    const loginButton = screen.getByRole('button', { name: /entrar/i });
+    userEvent.click(loginButton);
+
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/carteira');
   });
 });
